e1270d9f6d29209dd603884d7c3dc6b9fae2d239
Jakob Nielsen (OFFICE) <jakobpn@microsoft.com>
2019-02-03 21:48:49 -0800

    OpenAPI demo functions

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,4 +1,69 @@
-﻿/**
+﻿const endPoint = "https://gateway.saxobank.com/sim";
+const accessToken =
+"eyJhbGciOiJFUzI1NiIsIng1dCI6IkQ2QzA2MDAwMDcxNENDQTI5QkYxQTUyMzhDRUY1NkNENjRBMzExMTcifQ.eyJvYWEiOiI3Nzc3NyIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoiT3ptMWk5M0QtLWdHdEFTYzRURGpxUT09IiwiY2lkIjoiT3ptMWk5M0QtLWdHdEFTYzRURGpxUT09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiNTAyN2I4ZTQ3ZmI4NDAwZWE4YzMzZDQzMjExZGMyMzYiLCJkZ2kiOiI4NCIsImV4cCI6IjE1NDkzNDE5NDQifQ.3JXChxvGjlFE535mlZ5V0XfCevhZMtpFY_--UtPTIsQbi8-eRrE3ZrNV0plMmw7eisvSjns1Iw1KTgAyI39v_g";
+
+function openApiGet(uri: string, parameterList: string): Promise<any[][]>
+{
+  return new Promise(function (resolve) {
+    var xhr = new XMLHttpRequest();
+    
+    var url = `${endPoint}${uri}`;
+    console.log(`OpenAPI Request: ${url}`);
+
+    //add handler for xhr
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        //return result back to Excel
+        const response = JSON.parse(xhr.responseText);
+        console.log(`OpenAPI: ${JSON.stringify(response)}`);
+        const result = formatResult(response, parameterList);
+        console.log(`Result: ${JSON.stringify(result)}`);
+        resolve(result);
+      }
+    };
+
+    //make request
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Authorization",`Bearer ${accessToken}`);
+    xhr.send();
+  });
+}
+
+function formatResult(data: object, parametersList: string): any[][] {
+  const parameters = parametersList.split(",");
+  const header = parameters.map((p) => p.trim().replace(/.*\./, ""));
+  const body = data["Data"].map((item) => parameters.map((p) => eval(`item.${p}`)));
+
+  return [header].concat(body);
+}
+
+function stockQuote(ticker: string): Promise<number> {
+  //1. If(web call), you should want to return a "Promise".
+  //2. This tells Excel to #GETTING_DATA, until the promise is 'resolved'.
+  //3. NEW: Custom Functions allow you the user to continue interaction
+  return new Promise(function(resolve) {
+    var xhr = new XMLHttpRequest();
+
+    var url = "https://api.iextrading.com/1.0/stock/" + ticker + "/price";
+
+    //add handler for xhr
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        //return result back to Excel
+        var price = parseFloat(xhr.responseText);
+
+        resolve(price);
+      }
+    };
+
+    //make request
+    xhr.open("GET", url, true);
+    xhr.send();
+  });
+}
+
+
+/**
  * Adds two numbers.
  * @param first First number
  * @param second Second number
@@ -63,6 +128,8 @@ function logMessage(message: string): string {
  * Defines the implementation of the custom functions
  * for the function id defined in the metadata file (functions.json).
  */
+CustomFunctions.associate("GET", openApiGet);
+CustomFunctions.associate("STOCKQUOTE", stockQuote);
 CustomFunctions.associate("ADD", add);
 CustomFunctions.associate("CLOCK", clock);
 CustomFunctions.associate("INCREMENT", increment);
